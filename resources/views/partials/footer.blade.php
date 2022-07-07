3296cde575c64dc7136b370e38662c364eced489
LeleGuidi <emanuele.guidi98@outlook.it>
2022-07-07 15:45:48 +0200

    creazione di partials, style e base layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,67 @@
+<footer class="footer">
+    <div class="footer_center">
+        <div class="footer_center_row">
+            <div class="col_left">
+                <nav class="bottom_nav">
+                    <h2>dc comics</h2>
+                    <ul>
+                        <li><a href="#/">Characters</a></li>
+                        <li><a href="#/">Comics</a></li>
+                        <li><a href="#/">Movies</a></li>
+                        <li><a href="#/">TV</a></li>
+                        <li><a href="#/">Games</a></li>
+                        <li><a href="#/">Videos</a></li>
+                        <li><a href="#/">News</a></li>
+                    </ul>
+                </nav>
+                <nav class="bottom_nav">
+                    <h2>shop</h2>
+                    <ul>
+                        <li><a href="#/">Shop DC</a></li>
+                        <li><a href="#/">Shop DC Collectibles</a></li>
+                    </ul>
+                </nav>
+                <nav class="bottom_nav">
+                    <h2>dc</h2>
+                    <ul>
+                        <li><a href="#/">Term Of Use</a></li>
+                        <li><a href="#/">Privacy policy (New)</a></li>
+                        <li><a href="#/">Ad Choice</a></li>
+                        <li><a href="#/">Advertising</a></li>
+                        <li><a href="#/">Jobs</a></li>
+                        <li><a href="#/">Subscriprions</a></li>
+                        <li><a href="#/">Talent Workshops</a></li>
+                        <li><a href="#/">CPSC Certificates</a></li>
+                        <li><a href="#/">Ratings</a></li>
+                        <li><a href="#/">Shop Help</a></li>
+                        <li><a href="#/">Contact US</a></li>
+                    </ul>
+                </nav>
+                <nav class="bottom_nav">
+                    <h2>sites</h2>
+                    <ul>
+                        <li><a href="#/">DC</a></li>
+                        <li><a href="#/">MAD Magazine</a></li>
+                        <li><a href="#/">DC Kids</a></li>
+                        <li><a href="#/">DC Universe</a></li>
+                        <li><a href="#/">DC Power Visa</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col_right">
+                <img src="../../assets/img/dc-logo-bg.png" alt="logo dc">
+            </div>
+        </div>
+    </div>
+    <div class="footer_bottom">
+        <div class="footer_bottom_row">
+            <div class="col_left">
+                <button class="sign_btn">Sign-up now!</button>
+            </div>
+            <div class="col_right">
+                <h3>Follow us</h3>
+                <a v-for="(social, index) in socials" :key="index" href="#/"><img :src="social.url" :alt="social.name"></a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header class="header">
+    <div class="header_container">
+        <div class="header_logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="DC LOGO">
+        </div>
+        <div class="header_nav">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/#">Characters</a>
+                    </li>
+                    <li class="active">
+                        <a href="/#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="/#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="/#">TV</a>
+                    </li>
+                    <li>
+                        <a href="/#">Games</a>
+                    </li>
+                    <li>
+                        <a href="/#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="/#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="/#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="/#">News</a>
+                    </li>
+                    <li>
+                        <a href="/#">Shop</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
